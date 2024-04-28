4134cfbb3953ffcfe3e229ee8d066994a285611f
NataliaG1711 <157080304+NataliaG1711@users.noreply.github.com>
2024-04-28 13:09:10 -0500

    se agregan elementos a la funcionalidad

--- index.php
+++ index.php
@@ -1,56 +1,175 @@
-<?php
-    session_start();
-    if(isset($_SESSION['usuario'])){
-        header("Location: ../SABORES COLOMBIA/pantallainicio.php");
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Y Registro - Kevin López</title>
-    <link rel="stylesheet" href="assets/css/estilos.css">
+    <title>Sabores Colombia</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+    </style>
+    <link rel="stylesheet" href="estilos.css">
 </head>
+
 <body>
+    <header>
+        <h1><img src="imagenes/Saborescolombia.png" alt="logo de SaboresColombia"></h1>
+        <div class="cajaHeader">
+            <nav>
+                <ul>
+                    <li><a href="pantallaInicio.php">Inicio</a></li>
+                    <li><a href="index.php">INGREDIENTES</a></li>
+                    <li><a href="recetasInternacionales.php">RECETAS INTERNACIONALES</a></li>
+                    <li><a href="../CAJA COMENTARIOS/index2.php">FORO</a></li>
+                    <li><a href="../index.php" class="btn-1">LOGIN</a></li>
+                    <li><a href ="../bienvenida.php">Cerrar sesión</a><li></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 
     <main>
-        <div class="contenedor__todo">
-            <div class="caja__trasera">
-                <div class="caja__trasera-login">
-                    <h3>¿Ya tienes una cuenta?</h3>
-                    <p>Inicia sesión para entrar en la página</p>
-                    <button id="btn__iniciar-sesion">Iniciar sesión</button>
-                </div>
-                <div class="caja__trasera-register">
-                    <h3>¿Aún no tienes una cuenta?</h3>
-                    <p>Registrate para que puedas iniciar sesión</p>
-                    <button id="btn__registrarse">Registrarse</button>
-                </div>
-            </div>
+        <ul class="productos">
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'huevo')">
+                <h2>Huevo</h2>
+                <img src="imagenes/egg.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'arroz')">
+                <h2>Arroz</h2>
+                <img src="imagenes/rice.png">
 
-            <div class="contenedor__login-register">
-                <form action="php/login_usuario_be.php" method ="POST" 
-                class="formulario__login">
-                    <h2>Iniciar sesión</h2>
-                    <input type="text" placeholder="Correo electronico" name="correo">
-                    <input type="password" placeholder="contraseña" name="contrasena">
-                    <button>Entrar</button>
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'pechuga')">
+                <h2>Pollo</h2>
+                <img src="imagenes/pechuga.png">
 
-                    
-                </form>
-                <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
-                    <h2>Registrarse</h2>
-                    <input type="text" placeholder="Nombre Completos" name = "nombre_completo">
-                    <input type="text" placeholder="Correo electronico" name = "correo">
-                    <input type="text" placeholder="Usuario" name = "usuario">
-                    <input type="password" placeholder="contraseña" name = "contrasena">
-                    <button>Registrarse</button>
-                </form>
-            </div>
-        </div>
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'harina')">
+                <h2>Harina</h2>
+                <img src="imagenes/flour.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'cebolla')">
+                <h2>Cebolla</h2>
+                <img src="imagenes/onion.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'tomate')">
+                <h2>Tomate</h2>
+                <img src="imagenes/tomato.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'papas')">
+                <h2>Papas</h2>
+                <img src="imagenes/potato.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'leche')">
+                <h2>Leche</h2>
+                <img src="imagenes/milk.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'platano')">
+                <h2>Platano</h2>
+                <img src="imagenes/banana.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'carne')">
+                <h2>Carne</h2>
+                <img src="imagenes/meat.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'zanahoria')">
+                <h2>Zanahoria</h2>
+                <img src="imagenes/carrot.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'aguacate')">
+                <h2>Aguacate</h2>
+                <img src="imagenes/aguacate.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'lechuga')">
+                <h2>Lechuga</h2>
+                <img src="imagenes/lechuga.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'manzana')">
+                <h2>Manzana</h2>
+                <img src="imagenes/manzana.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'yuca')">
+                <h2>Yuca</h2>
+                <img src="imagenes/yuca.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'azucar')">
+                <h2>Azúcar</h2>
+                <img src="imagenes/Azucar.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'sal')">
+                <h2>Sal</h2>
+                <img src="imagenes/Sal.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'pimienta')">
+                <h2>Pimienta</h2>
+                <img src="imagenes/Pimienta.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'aceite')">
+                <h2>Aceite</h2>
+                <img src="imagenes/oil.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'ajo')">
+                <h2>Ajo</h2>
+                <img src="imagenes/ajo.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'pimentonRojo')">
+                <h2>Pimenton Rojo</h2>
+                <img src="imagenes/pimentonRojo.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'pimentonVerde')">
+                <h2>Pimenton Verde</h2>
+                <img src="imagenes/pimentonVerde.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'pimentonAmarillo')">
+                <h2>Pimenton Amarillo</h2>
+                <img src="imagenes/pimentonAmarillo.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'cebollaLarga')">
+                <h2>Cebolla Larga</h2>
+                <img src="imagenes/cebollaLarga.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'Cilantro')">
+                <h2>Cilantro</h2>
+                <img src="imagenes/cilantro.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'mantequilla')">
+                <h2>Mantequilla</h2>
+                <img src="imagenes/mantequilla.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'chorizo')">
+                <h2>Chorizo</h2>
+                <img src="imagenes/chorizo.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'frijoles')">
+                <h2>Frijoles</h2>
+                <img src="imagenes/frijoles.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'queso')">
+                <h2>Queso</h2>
+                <img src="imagenes/queso.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'almidonYuca')">
+                <h2>Almidon de Yuca</h2>
+                <img src="imagenes/almidonYuca.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'mazorca')">
+                <h2>Mazorca</h2>
+                <img src="imagenes/mazorca.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'migaPan')">
+                <h2>Miga de Pan</h2>
+                <img src="imagenes/migaPan.png">
+            </li>
+            <li onclick="changeBorderColor(this); selectIngredient(this, 'chicharron')">
+                <h2>Chicharron</h2>
+                <img src="imagenes/chicharron.png">
+            </li>
+        </ul>
+        
+        <button onclick="window.location.href = 'recetas.php';"  class="btnSiguiente">Siguiente</button>
     </main>
-    <script src ="assets/js/script.js"></script>
+    <script src="home.js"></script>
+    <script src="ingredientes.js"></script>
 </body>
+
 </html>
---
+++ ingredientes.js
@@ -0,0 +1,120 @@
+
+let selectedIngredients = [];
+
+function selectIngredient(element, ingredientName) {
+    element.classList.toggle('selected');
+    const index = selectedIngredients.indexOf(ingredientName);
+    if (index === -1) {
+        selectedIngredients.push(ingredientName);
+    } else {
+        selectedIngredients.splice(index, 1);
+    }
+    localStorage.setItem('selectedIngredients', JSON.stringify(selectedIngredients));
+    mostrarRecetas();
+    console.log(selectedIngredients);
+}
+
+function cargarIngredientesSeleccionados() {
+    const storedIngredients = localStorage.getItem('selectedIngredients');
+    if (storedIngredients) {
+        selectedIngredients = JSON.parse(storedIngredients);
+        mostrarRecetas();
+    }
+    document.querySelectorAll('.recetas li').forEach(function(receta) {
+        receta.style.display = 'none';
+        
+    });
+    console.log(selectedIngredients);
+}
+
+cargarIngredientesSeleccionados();
+console.log(selectedIngredients);
+
+function mostrarRecetas() {
+    console.log(selectedIngredients)
+    const arrozConPollo = document.getElementById('arrozConPollo');
+    const bandejaPaisa = document.getElementById('bandejaPaisa');
+    const changua = document.getElementById('changua')
+    const empanada = document.getElementById('empanada');
+    const pandebono = document.getElementById('pandebono');
+    const sancocho = document.getElementById('sancocho');
+    const arepaDeHuevo = document.getElementById('arepaDeHuevo');
+    const ajiaco = document.getElementById('ajiaco');
+    const papaRellena = document.getElementById('papaRellena');
+    const huevoPerico = document.getElementById('huevoPerico');
+    const tamales = document.getElementById('tamales');
+    const arepaDeQueso = document.getElementById('arepaDeQueso');
+    const salchipapa = document.getElementById('salchipapa');
+
+    if (selectedIngredients.includes('pechuga') && selectedIngredients.includes('arroz') && selectedIngredients.includes('cebolla') && selectedIngredients.includes('tomate')){
+        arrozConPollo.style.display = 'inline-block';
+    } else {
+        arrozConPollo.style.display = 'none';
+    }
+    if(selectedIngredients.includes('chicharron') && selectedIngredients.includes('huevo') && selectedIngredients.includes('carne') && selectedIngredients.includes('frijoles') && selectedIngredients.includes('aguacate') && selectedIngredients.includes('platano')){
+        bandejaPaisa.style.display = 'inline-block';
+    } else {
+        bandejaPaisa.style.display = 'none';
+    }
+    if(selectedIngredients.includes('huevo') && selectedIngredients.includes('leche') && selectedIngredients.includes('cebollaLarga')){
+        changua.style.display = 'inline-block';
+    } else{
+        changua.style.display = 'none';
+    }
+    if(selectedIngredients.includes('harina') && selectedIngredients.includes('carne') && selectedIngredients.includes('papas')){
+        empanada.style.display = 'inline-block';
+    } else{
+        empanada.style.display = 'none';
+    }
+    if(selectedIngredients.includes('almidonYuca') && selectedIngredients.includes('leche') && selectedIngredients.includes('queso') && selectedIngredients.includes('huevo')){
+        pandebono.style.display = 'inline-block';
+    } else{
+        pandebono.style.display = 'none';
+    }
+    if(selectedIngredients.includes('carne') && selectedIngredients.includes('yuca') && selectedIngredients.includes('platano') && selectedIngredients.includes('papa') && selectedIngredients.includes('cebolla')){
+        sancocho.style.display = 'inline-block';
+    } else{
+        sancocho.style.display = 'none';
+    }
+    if(selectedIngredients.includes('huevo') && selectedIngredients.includes('harina')){
+        arepaDeHuevo.style.display = 'inline-block';
+    } else{
+        arepaDeHuevo.style.display = 'none';
+    }
+    if(selectedIngredients.includes('pechuga') && selectedIngredients.includes('papas') && selectedIngredients.includes('mazorca')){
+        ajiaco.style.display = 'inline-block';
+    } else{
+        ajiaco.style.display = 'none';
+    }
+    if(selectedIngredients.includes('papas') && selectedIngredients.includes('carne') && selectedIngredients.includes('cebolla') && selectedIngredients.includes('huevo') && selectedIngredients.includes('harina')){
+        papaRellena.style.display = 'inline-block';
+    } else{
+        papaRellena.style.display = 'none';
+    }
+    if(selectedIngredients.includes('huevo') && selectedIngredients.includes('tomate') && selectedIngredients.includes('cebolla')){
+        huevoPerico.style.display = 'inline-block';
+    } else{
+        huevoPerico.style.display = 'none';
+    }
+    if(selectedIngredients.includes('pechuga') && selectedIngredients.includes('carne') && selectedIngredients.includes('harina') && selectedIngredients.includes('huevo')){ 
+        tamales.style.display = 'inline-block';
+    } else{
+        tamales.style.display = 'none';
+    }
+    if(selectedIngredients.includes('harina') && selectedIngredients.includes('queso')){
+        arepaDeQueso.style.display = 'inline-block';
+    } else{
+        arepaDeQueso.style.display = 'none';
+    }
+    if(selectedIngredients.includes('chorizo') && selectedIngredients.includes('papas')){
+        salchipapa.style.display = 'inline-block';
+    } else{
+        salchipapa.style.display = 'none';
+    }
+}
+
+document.querySelectorAll('a').forEach(function(enlace) {
+    enlace.addEventListener('click', function() {
+        localStorage.removeItem('selectedIngredients');
+    });
+});
